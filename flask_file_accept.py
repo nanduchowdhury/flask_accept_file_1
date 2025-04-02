091a87af9aded625c98c0a2a0b936284a341c173
nanduchowdhury <nandu.chowdhury@gmail.com>
2025-04-02 11:15:11 +0000

    - turn off debug in Flask

--- flask_file_accept.py
+++ flask_file_accept.py
@@ -952,5 +952,5 @@ app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024  # 32 MB
 Session(app)
 
 if __name__ == '__main__':
-  app.run(debug=True, threaded=True)
+  app.run(debug=False, threaded=True)
 
